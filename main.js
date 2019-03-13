9fa0d8b47f1f076c3f9165064b33b385171588c4
anilbadatiya <anil.badatiya@gmail.com>
2019-03-13 14:17:08 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
    // Initialize Firebase
         // TODO: Replace with your project's customized code snippet
         var config = {
-            apiKey: "AIzaSyAUV-qA7V6y3t_X0ATrFgv_E0E7kNSjxHA",
-            authDomain: "aniltest-9777a.firebaseapp.com",
-            databaseURL: "https://aniltest-9777a.firebaseio.com",
-            projectId: "aniltest-9777a",
-            storageBucket: "aniltest-9777a.appspot.com/-L_kqUmVZCbRg-vhrQC3/Team-Awesome/Members/.json",
-            messagingSenderId: "306496296920"
+            apiKey: "",
+            authDomain: "",
+            databaseURL: "",
+            projectId: "",
+            storageBucket: "",
+            messagingSenderId: ""
         };
         firebase.initializeApp(config);
       
